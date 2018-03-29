c2faf9844e270afe6dc9e8980d4a5eafbb290338
lanzhihao1995 <37768727+lanzhihao1995@users.noreply.github.com>
2018-03-29 22:08:40 +0800

    upload project
    上传项目

---
+++ Assets/Topdown Kit/Script/Base/IController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IController {
+
+    /// <summary>
+    /// 显示模块
+    /// </summary>
+    void Show( string res_path );
+    /// <summary>
+    /// 隐藏模块
+    /// </summary>
+    void Hide();
+    /// <summary>
+    /// 是否显示
+    /// </summary>
+    bool IsShow();
+    /// <summary>
+    /// 设置模块
+    /// </summary>
+    void SetView(IView view);
+    /// <summary>
+    /// 获取模块
+    /// </summary>
+    void GetView();
+    /// <summary>
+    /// 初始化协议
+    /// </summary>
+    void InitServer();
+    /// <summary>
+    /// 初始化模块
+    /// </summary>
+    void InitView();
+}
---
+++ Assets/Topdown Kit/Script/MVC/Base.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour {
+
+    private UIManager _uiManager;
+
+
+    protected UIManager UIManager {
+        get {
+            if ( !_uiManager ) {
+                _uiManager = new UIManager();
+            }
+            return _uiManager;
+        }
+    }
+	
+}
