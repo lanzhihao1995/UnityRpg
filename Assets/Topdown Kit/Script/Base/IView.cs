53af196ed879422a757ae17772a13e47046e8b18
lanzhihao1995 <37768727+lanzhihao1995@users.noreply.github.com>
2018-03-30 23:59:33 +0800

    Revert "Revert "Revert "Revert "upload project""""
    This reverts commit 37c52ef0dc348abc2ffa4d73e83d241121e89717.

---
+++ Assets/Topdown Kit/Script/Base/IView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IView{
+
+    /// <summary>
+    /// 显示界面
+    /// </summary>
+    /// <param name="res_path">资源路径</param>
+    void Show( string res_path );
+    /// <summary>
+    /// 隐藏界面
+    /// </summary>
+    void Hide();
+    /// <summary>
+    /// 界面是否显示
+    /// </summary>
+    bool IsShow();
+    /// <summary>
+    /// 显示或者隐藏
+    /// </summary>
+    void ShowOrHide();
+    /// <summary>
+    /// 销毁界面
+    /// </summary>
+    void Dispose();
+}
---
+++ Assets/Topdown Kit/Script/UI/Login/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginController : Controller {
+
+    public override void InitServer() {
+        throw new System.NotImplementedException();
+    }
+
+    public override void InitView() {
+        IView view = new LoginModule();
+        this.SetView( view );
+    }
+}
